d0dab1669bf40641361cb28c7b3add8751376bb0
carmipa <rm557881@fiap.com.br>
2025-08-07 22:57:49 -0300

    botão e imagens com nova entrada

--- aulas/AulaTanQuery1/src/HomeScreen.js
+++ aulas/AulaTanQuery1/src/HomeScreen.js
@@ -1,12 +1,25 @@
-import {View, Text, StyleSheet, FlatList, ActivityIndicator} from "react-native";
-import {useQuery} from '@tanstack/react-query' //hook para fazer queries
-import {fetchUsers} from "../api/api.js" // função de requisição
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, Button, Image } from "react-native";
+import { useQuery, useMutation } from '@tanstack/react-query' //hook para fazer queries
+import { fetchUsers, createUser } from "./api/api.js" // função de requisição
+import { SafeAreaView} from "react-native-safe-area-context";
 
 export default function HomeScreen(){
-    const{data, isLoading, isError, error} = useQuery({
+    const{data, isLoading, isError, error, isFetching, refetch} = useQuery({
         queryKey: ['users'],
         queryFn: fetchUsers
     })
+
+    // criando mutation para enviar um novo usuário
+    const mutation = useMutation({
+        mutationFn:createUser,
+        onSuccess:()=>refetch() // atualiza a lista após a criação do usuário
+    })
+
+    // usuario a ser enviado
+    const newUser = {
+        name: "Gundam",
+        avatar: "https://upload.wikimedia.org/wikipedia/en/d/d6/RX-78-2_Gundam_illustration.gif"
+    }
 
     //exibe um spinner durante o carregamento dos dados
     if(isLoading){
@@ -24,13 +37,47 @@ export default function HomeScreen(){
     }
 
     return(
-        <FlatList
-            data={data}
-            renderItem={({item})=>(
-                <View style={styles.item}>
-                    <Text style={styles.title}>{item.name}</Text>
-                </View>
-            )}
-        />
+       <SafeAreaView>
+            <Button
+                title={mutation.isPending?"Criando usuário..." : "Criar novo usuário"}
+                onPress={()=>mutation.mutate(newUser)} // envio newuser para api
+            />
+            <FlatList
+                data={data}
+                refreshing={isFetching} // mostrar o spinner durante o refect
+                onRefresh={refetch} // chamada automatica do refct atualização
+                renderItem={({item})=>(
+                    <View style={styles.item}>
+                        <Image source={{ uri: item.avatar }} style={styles.avatar} />
+                        <Text style={styles.title}>{item.name}</Text>
+                    </View>
+                )}
+            />
+           {console.log(data)}
+       </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    center:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    item:{
+        padding:16,
+        borderBottomWidth:1,
+        borderBottomColor:`#ccc`
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#eee'
+    },
+    title:{
+        fontWeight:'bold',
+        marginBottom:4
+    }
+
+})
--- aulas/AulaTanQuery1/src/QueryClientProvider.js
+++ aulas/AulaTanQuery1/src/QueryClientProvider.js
@@ -1,10 +1,11 @@
 // Componentes do TanQuery
+// é o arquivo que criamos para que a aplicação seja envolvida e tenha acesso ao tanquerry
 
-import {QueryCliente, QueryClientProvider as TanstackProvider } from "./QueryCliente.js";
+import {QueryClient, QueryClientProvider as TanstackProvider } from "@tanstack/react-query";
 
 // cria uma instnacia do queryClient (controlar erros, loading )
 
-const queryClient = new QueryCliente();
+const queryClient = new QueryClient();
 
 // um componente que ira envolver a aplicação e permitir que qualquer componebte filho acesse o tanquery
 
